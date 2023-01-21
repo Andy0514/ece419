80ed34acbc567b77c5b72197c1098292644492f4
Yun-Hsiang Chan <rayhs.chan@mail.utoronto.ca>
2023-01-20 22:58:40 -0500

    redo server preliminary implementation

--- M1/src/app_kvServer/KVServer.java
+++ M1/src/app_kvServer/KVServer.java
@@ -1,6 +1,17 @@
 package app_kvServer;
 
-public class KVServer implements IKVServer {
+import logger.LogSetup;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import server.ClientConnection;
+import server.Server;
+
+import java.io.IOException;
+import java.net.BindException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class KVServer extends Thread implements IKVServer {
 	/**
 	 * Start KV Server at given port
 	 * @param port given port for storage server to operate
@@ -11,14 +22,33 @@ public class KVServer implements IKVServer {
 	 *           currently not contained in the cache. Options are "FIFO", "LRU",
 	 *           and "LFU".
 	 */
+	private int port;
+	private int cacheSize;
+	private String strategy;
+
+	/**
+	 * @param logger logger for the server.
+	 * @param running boolean value assigned during the sever initialization, which represents whether the server is running or not.
+	 * @param severSocket the server side of a socket connection    .
+	 *
+	 * **/
+	private static Logger logger = Logger.getRootLogger();
+	private boolean running;
+	private ServerSocket serverSocket;
+
+
 	public KVServer(int port, int cacheSize, String strategy) {
 		// TODO Auto-generated method stub
+		this.port = port;
+		this.cacheSize = cacheSize;
+		this.strategy = strategy;
 	}
-	
+
 	@Override
 	public int getPort(){
 		// TODO Auto-generated method stub
-		return -1;
+		return this.port;
+//		return -1;
 	}
 
 	@Override
@@ -36,7 +66,8 @@ public class KVServer implements IKVServer {
 	@Override
     public int getCacheSize(){
 		// TODO Auto-generated method stub
-		return -1;
+		return this.cacheSize;
+//		return -1;
 	}
 
 	@Override
@@ -72,18 +103,78 @@ public class KVServer implements IKVServer {
 		// TODO Auto-generated method stub
 	}
 
+	private boolean initializeServer() {
+		logger.info("Initialize server ...");
+		try {
+			serverSocket = new ServerSocket(this.port);
+			logger.info("Server listening on the port: " + serverSocket.getLocalPort());
+			return true;
+		} catch (IOException e) {
+			logger.error("Error! Cannot open server socket");
+			if (e instanceof BindException) {
+				logger.error("Port " + port + " is already bound!");
+			}
+			return false;
+		}
+	}
+
+	private boolean isRunning() { return this.running;}
+
 	@Override
     public void run(){
 		// TODO Auto-generated method stub
+		running = initializeServer();
+
+		if (serverSocket != null) {
+			while (isRunning()) {
+				try {
+					Socket client = serverSocket.accept();
+					ClientConnection connection = new ClientConnection(client);
+					new Thread(connection).start();
+
+					logger.info("Connected to "
+							+ client.getInetAddress().getHostName()
+							+ " on port " + client.getPort());
+				} catch (IOException e) {
+					logger.error("Error! " + "Unable to establish connection. \n ", e);
+				}
+			}
+		}
+
+		logger.info("Server stopped.");
 	}
 
 	@Override
     public void kill(){
 		// TODO Auto-generated method stub
+
 	}
 
 	@Override
     public void close(){
 		// TODO Auto-generated method stub
+	}
+
+	public static void main(String[] args) {
+		try {
+			new LogSetup("logs/server.log", Level.ALL);
+			if (args.length != 3) {
+				System.out.println("Error! Invalid number of arguments!");
+				System.out.println("Usage: Server <port> <cacheSize> <strategy>");
+			} else {
+				int port = Integer.parseInt(args[0]);
+				int cacheSize = Integer.parseInt(args[1]);
+				String strategy = args[2];
+				new KVServer(port, cacheSize, strategy).start();
+			}
+		} catch (IOException e) {
+			System.out.println("Error! Unable to initialize logger!");
+			e.printStackTrace();
+			System.exit(1);
+		} catch (NumberFormatException nfe) {
+			System.out.println("Error! Invalid argument <port> or <cacheSize>! Not a number!");
+			System.out.println("Usage: Server <port> <cacheSize> <strategy>");
+			System.exit(1);
+		}
 	}
 }
